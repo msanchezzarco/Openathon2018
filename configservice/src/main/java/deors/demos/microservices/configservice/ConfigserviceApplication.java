5008fc8f8cb70b7fb8d6efaf17fa12da1de3363f
Sánchez Zarco <m.sanchez.zarco@accenture.com>
2018-06-08 16:42:17 +0200

    initial configuration

---
+++ configservice/src/main/java/deors/demos/microservices/configservice/ConfigserviceApplication.java
@@ -0,0 +1,12 @@
+package deors.demos.microservices.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserviceApplication.class, args);
+	}
+}
